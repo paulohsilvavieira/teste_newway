1b4a38208f62319c025234d8d96c9c8f3b099ad4
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2025-05-19 14:06:15 -0300

    chore: create terraform files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,8 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -11,8 +13,20 @@ import { TaskModule } from './task/task.module';
     }),
     UserModule,
     TaskModule,
+     ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
   ],
   controllers: [],
-  providers: [],
+  providers: [ {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }
+  ],
 })
 export class AppModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import helmet from 'helmet';
 async function bootstrap() {
   const { setupSagger } = await import('./swagger');
 
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-
+  app.enableCors({
+     origin: process.env.ORIGIN_CORS,
+  });
+  app.use(helmet);
   setupSagger(app);
   await app.listen(process.env.PORT ?? 3001);
 }
